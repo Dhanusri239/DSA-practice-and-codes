3170923fff0ea205ad08462a52e7189d02ba7feb
Dhanusri239 <sdhanu946@gmail.com>
2025-12-11 16:00:51 +0530

    Anagram code

---
+++ anagram.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+public class anagram {
+    public static boolean f(String s,String t){
+        if(s.length()==t.length()){
+            int a[]=new int[26];
+            for(int i=0;i<s.length();i++){
+                a[s.charAt(i)-'a']++;
+                a[t.charAt(i)-'a']--;
+            }
+            for(int c:a){
+                if(c!=0)return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String t=sc.nextLine();
+        boolean r=f(s,t);
+        System.out.println(r);
+    }
+}
